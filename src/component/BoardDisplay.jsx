91885db4242b452d16abb31590635724b5e1f427
Steven Dickinson <github@orangestems.co.uk>
2015-09-27 17:26:10 +0100

    Highlight moves on hovered automaton

--- src/component/BoardDisplay.jsx
+++ src/component/BoardDisplay.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { Coordinate } from '../game/entities.js';
+import { Map } from 'immutable';
 
 export default class extends React.Component {
     render() {
-        const automatonPositions = this.props.board.automatons.map((automaton) => automaton.position).toSet();
+        const { board, hoverAutomaton } = this.props;
+        
+        let positionClasses = Map(
+            board.automatons.map((automaton) => [automaton.position, 'automaton'])
+        );
+
+        if (hoverAutomaton) {
+            positionClasses = positionClasses.set(hoverAutomaton.position, 'automaton active');
+
+            window.console.log('all moves', board.getAvailableMoves().toJS());
+
+            board.getAvailableMoves()
+                .filter((move) => move.automaton.equals(hoverAutomaton))
+                .forEach((move) => positionClasses = positionClasses.set(move.nextPosition, 'move'))
+            ;
+            window.console.log("classes", positionClasses.toJS());
+
+        }
+
 
         let rows = [];
-        for(let y = 0; y < this.props.board.dim.y; y++) {
+        for(let y = 0; y < board.dim.y; y++) {
             let cells = [];
-            for(let x = 0; x < this.props.board.dim.x; x++) {
+            for(let x = 0; x < board.dim.x; x++) {
                 const coord = Coordinate({x, y});
-                const hasAutomaton = automatonPositions.has(coord);
-                const onMouseOver = hasAutomaton ?
-                    () => this.props.onHoverAutomaton(this.props.board.automatons.find((a) => a.position.equals(coord)))
-                    : null;
-                const onMouseOut = hasAutomaton ?
-                    () => this.props.onHoverAutomaton(null)
-                    : null;
-                let className = hasAutomaton ? 'active' : '';
-                if (this.props.hoverAutomaton && this.props.hoverAutomaton.position.equals(coord)) {
-                    className += ' hover';
-                }
+                const onMouseOver = () => this.props.onHoverAutomaton(board.automatons.find((a) => a.position.equals(coord)))
+                const onMouseOut =  () => this.props.onHoverAutomaton(null)
+
+                let className = positionClasses.get(Coordinate({x, y}));
 
                 cells.push(
                     <span key={x} className={className}

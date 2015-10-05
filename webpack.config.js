c8009c5545cc363819eb846a544760d5e9df64ef
Steven Dickinson <github@orangestems.co.uk>
2015-10-05 21:12:01 +0100

    Further prep, rename existing page

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         publicPath: '/static/'
     },
     plugins: [
